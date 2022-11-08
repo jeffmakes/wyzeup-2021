a853efe061d7890553bad4bb5749d419c8ea677e
dh_spdipt <dh_spdipt@gordon.dreamhost.com>
2022-11-07 17:08:41 -0800

    Fix mysql queries - can now click the Un/Wyze buttons and progress

--- scenario_score_demo.php
+++ scenario_score_demo.php
@@ -7,12 +7,20 @@
 	$quest_id 	= $_GET['questid'];
 	$display_id = $_GET['displayid'];
 	$flagvalue  = $_GET['fg'];
+    global $con;
 	
 	if($display_id  != 5){
 		$select_qry = "SELECT question_answer,question_image FROM wyzeup_questionsdemo WHERE  question_id=".$quest_id." AND question_isdeleted = 0";
-		$result = mysql_query($select_qry);
-		if(mysql_num_rows($result)){ 
-			$row = mysql_fetch_array($result); 
+		$result = mysqli_query($con, $select_qry);
+        error_log("just did query. result:...");
+        ob_start();
+        var_dump($result);
+        error_log(ob_get_clean());
+        error_log("Rows:");
+        error_log(mysqli_num_rows($result));
+
+		if(mysqli_num_rows($result)){ 
+			$row = mysqli_fetch_array($result); 
 			if($row['question_answer'] == $value){ 
 					if($flagvalue == 1){ 
 						$j = $display_id;
@@ -24,8 +32,8 @@
 					$flag=1; 
 					$correct= "Yes";
 					$query = "SELECT score_id,score_image,score_value FROM wyzeup_score WHERE score_value=".$j." AND score_isdeleted = 0 ";
-					$qry_result = mysql_query($query);
-					$row_img = mysql_fetch_array($qry_result);
+					$qry_result = mysqli_query($con, $query);
+					$row_img = mysqli_fetch_array($qry_result);
 				echo $row_img["score_image"]."@##@".$correct."@##@".$j."@##@".$flag."@##@".$value;		
 			}
 			else{
@@ -39,8 +47,8 @@
 					$flag=1;
 					$correct= "No";
 					$query = "SELECT score_id,score_image,score_value FROM wyzeup_score WHERE score_value=".$j." AND score_isdeleted = 0 ";
-					$qry_result = mysql_query($query);
-					$row_img = mysql_fetch_array($qry_result);
+					$qry_result = mysqli_query($con, $query);
+					$row_img = mysqli_fetch_array($qry_result);
 				echo $row_img["score_image"]."@##@".$correct."@##@".$j."@##@".$flag."@##@".$value;			
 			}
 		}
